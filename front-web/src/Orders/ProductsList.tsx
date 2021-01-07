fc5858703d72177ab82bbb17d9653727280aa92a
Bruno Alves <brunovicentealves.ti@gmail.com>
2021-01-07 17:51:41 -0300

    product list integration api backend

---
+++ front-web/src/Orders/ProductsList.tsx
@@ -0,0 +1,21 @@
+import ProductCard from "./ProductCard";
+import { Product } from "./types";
+
+type Props = {
+    product: Product[];
+}
+
+function ProductsList({product}:Props) {
+    return (
+        <div className="orders-list-container">
+            <div className="orders-list-items">
+                {product.map(product => (
+                    <ProductCard key={product.id} product={product}/>
+                    
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default ProductsList;
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react';
+import { isConstructorDeclaration } from 'typescript';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css';
+import { Product } from './types';
 
 
 function Orders() {
+
+ const [product, setProducts]=useState<Product[]>([])
+
+   useEffect(() => {
+     fetchProducts().then(
+        Response => setProducts(Response.data))
+        .catch(error => console.log(error))
+
+   }, [])
+
+
    return (
 
       <div className="orders-container">
 
-         <StepsHeaders/>
-        
+         <StepsHeaders />
+         <ProductsList product={product} />
+
 
       </div>
    )
