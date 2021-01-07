5f68c60dfa2102961d61b73c450ebc16c518a44d
Bruno Alves <brunovicentealves.ti@gmail.com>
2021-01-06 23:04:27 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
+import { homedir } from 'os';
 import './App.css';
 import Counter from './Counter';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <Home/>
     </div>
   );
 }
