bb070c24a216abb6d92046b0a3d06d79978d8c85
Bruno Alves <brunovicentealves.ti@gmail.com>
2021-01-09 22:12:44 -0300

    finish front web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Navbar from './Navbar';
 import Routes from './Routes';
-import { Toast } from 'react-toastify/dist/components';
+
 
 
 function App() {
