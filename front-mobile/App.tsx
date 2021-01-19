045bfffc4989d35b56c67b1c0eae05fd5d2b3ba9
Bruno Alves <brunovicentealves.ti@gmail.com>
2021-01-18 23:16:52 -0300

    add home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
+import Home from './src/Home';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
       <Header/>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
